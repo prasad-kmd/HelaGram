1781ee4b6053a9c9ea5fd900c5d5edb26f345e17
Prasad-Madhuranga <76102607+Prasad-Madhuranga@users.noreply.github.com>
2023-12-25 01:00:27 +0530

    Initial temp. release with new packages and some details.

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -8,9 +8,10 @@ const AuthLayout = () => {
       <Navigate to="/"/>
     ):(
       <>
-      <section>
+      <section className="flex flex-1 justify-center items-center flex-col py-10">
         <Outlet/>
       </section>
+      <img src="/assets/images/side-img.svg" alt="Sign in & Sign up Background LOGO" className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"/>
       </>
     )}
     </>
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,12 +1,154 @@
-import { Button } from "@/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
+import { useForm } from "react-hook-form";
+import { SignupValidation } from "@/lib/validation";
+import { z } from "zod";
+// import { Loader } from "lucide-react";
+import Loader from "@/components/shared/Loader";
+import { Link } from "react-router-dom";
+import { createUserAccount } from "@/lib/appwrite/api";
 
 const SignupForm = () => {
-  return (
-    <div>
-      <Button>Click me</Button>
-    </div>
-  )
-}
+  const isLoading = false;
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
+    defaultValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
 
-export default SignupForm
+  // 2. Define a submit handler.
+ async function onSubmit(values: z.infer<typeof SignupValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    const newUser = await createUserAccount(values);
+    console.log(newUser)
+  }
+  return (
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="/assets/images/logo.svg" alt="Main LOGO" />
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+          Create a New Account
+        </h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2">
+          To use HelaGram, please enter your account details
+        </p>
+
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-5 w-full mt-4"
+        >
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="HelaGram"
+                    type="text"
+                    className="shad-input"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  This wil be your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="@HelaGram"
+                    type="text"
+                    className="shad-input"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>This wil be your username.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="user@helagram.lk"
+                    type="email"
+                    className="shad-input"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>Enter your Email.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="********"
+                    type="password"
+                    className="shad-input"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>Choose a Password.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="shad-button_primary">
+            {isLoading ? (
+              <div className="flex-center gap-2">
+                <Loader />
+                Loading....
+              </div>
+            ) : (
+              "Sign up"
+            )}
+          </Button>
+          <p className="text-small-regular text-light-2 text-center mt-2">
+            Already have an account? <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1">Log in</Link>
+          </p>
+        </form>
+      </div>
+    </Form>
+  );
+};
+
+export default SignupForm;
