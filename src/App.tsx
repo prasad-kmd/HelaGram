bb33dab805f28b67c045a92a9815eaa7fecc94e2
Prasad-Madhuranga <76102607+Prasad-Madhuranga@users.noreply.github.com>
2023-12-24 19:14:46 +0530

    Initial 3rd Commit with New Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import './globals.css';
+import { Routes, Route } from "react-router-dom";
+
+import "./globals.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import { Home } from "./_root/pages";
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
-}
+    <main className="flex h-screen">
+      <Routes>
+        {/* { public routes} */}
+        <Route path="/sign-in" element={<SigninForm />} />
 
-export default App
+        {/* {private routes} */}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
